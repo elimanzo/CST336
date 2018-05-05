c4de90f1f11b997916f3a8415630e72b3c9e4ac8
Eli Manzo <elmanzo@csumb.edu>
2018-05-05 22:25:02 +0000

    lab10

---
+++ lab/lab10/about.php
@@ -0,0 +1,10 @@
+<?php include 'inc/header.php'; ?>
+<script>
+    $(document).ready(function(){
+        $("#home").removeClass("active");
+        $("#pets").removeClass("active");
+        $("#about").addClass("active");
+    });
+</script>
+<div class="display-4">Adopt a pet today and get a new best friend!</div>
+<?php include 'inc/footer.php'; ?>
