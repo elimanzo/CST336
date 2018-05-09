d767fe009a11a8a57db55410e182d299559f4768
Eli Manzo <elmanzo@csumb.edu>
2018-05-09 20:44:29 +0000

    Final Footer

--- projects/finalProject/inc/footer.php
+++ projects/finalProject/inc/footer.php
@@ -1,5 +1,6 @@
     <div class='text-center'>
     <hr>
-    <strong>Disclaimer:</strong> All information in this website is fake. <br />
+    <strong>Disclaimer:</strong> All information in this website is fictitious. <br />
+    The information on the website is used for academic purposes.<br />
     &copy;2018 Eli A. Manzo
     </div>
